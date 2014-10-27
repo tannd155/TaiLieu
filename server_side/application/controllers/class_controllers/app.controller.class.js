f0146aad715fe383a2d1f7caaca885f33d1fc1b8
tannd155 <tannd155@gmail.com>
2014-10-27 11:07:49 +0700

    asd

--- server_side/application/controllers/class_controllers/app.controller.class.js
+++ server_side/application/controllers/class_controllers/app.controller.class.js
@@ -3,4 +3,41 @@
     var classes                                     = require('../../models/app.model.class'),
         classModel                                  = classes.classModel,
         classMemberModel                            = classes.classMemberModel;
+    var classClass                                  = function(){
+    };
+    
+    classClass.prototype.getListClass = function(callback){
+        classModel.getListClass(callback);
+    };
+    
+    classClass.prototype.getClassByID = function(id, callback){
+        classModel.getClassByID(id, callback);
+    };
+    
+    classClass.prototype.addClass = function(class_name, term, year, callback) {
+        classModel.addClass(class_name, term, year, callback);
+    };
+    
+    classClass.prototype.updateClass = function(class_id, class_name, term, year, status, callback){
+        classModel.updateClass(class_id, class_name, term, year, status, callback);
+    };
+    
+    classClass.prototype.getMemberByID = function(id, callback) {
+        classMemberModel.getMemberByID(id, callback);
+    };
+    
+    classClass.prototype.getMemberByClassID = function(class_id, callback){
+        classMemberModel.getMemberByClassID(class_id, callback);
+    };
+    
+    classClass.prototype.addMemberToClass = function(class_id, user_id, callback){
+        classMemberModel.addMemberToClass(class_id, user_id, callback);
+    };
+    
+    classClass.prototype.updateMember = function(user_id, class_id, status, callback){
+        classMemberModel.updateMember(user_id, class_id, status, callback);
+    };
+    
+    exports.class = new classClass();
+    
 }).call(this);
